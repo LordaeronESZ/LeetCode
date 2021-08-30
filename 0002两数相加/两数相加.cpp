12e65146533a1e07fa73ff9507307041935ab8b6
unknown <1667510710@qq.com>
2021-08-30 20:43:07 +0800

    20210930

--- 0002两数相加/两数相加.cpp
+++ 0002两数相加/两数相加.cpp
@@ -12,68 +12,58 @@ struct ListNode
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* l3 = new ListNode();
-        ListNode* tempNode = &(*l3);
+        ListNode* headNode = nullptr;
+        ListNode* pathNode = nullptr;
         int c = 0;
-        while (l1->next && l2->next) {
-            int tempVal = l1->val + l2->val;
-            if (tempVal >= 10) {
-                tempVal -= 10;
-                c = 1;
+        while (l1 && l2)
+        {
+            int sum = l1->val + l2->val + c;
+            c = sum / 10;
+            if (!headNode) {
+                pathNode = new ListNode(sum % 10);
+                headNode = pathNode;
             }
             else {
-                c = 0;
+                pathNode->next = new ListNode(sum % 10);
+                pathNode = pathNode->next;
             }
-            if (l3 == tempNode)
-                tempNode->val = tempVal;
-            else
-                tempNode->val = tempVal + c;
-            tempNode->next = new ListNode();
-            tempNode = tempNode->next;
             l1 = l1->next;
             l2 = l2->next;
         }
-        while (l1->next) {
-            tempNode->next = new ListNode(l1->val + c);
-            c = 0;
-            tempNode = tempNode->next;
+        while (l1)
+        {
+            int sum = l1->val + c;
+            c = sum / 10;
+            pathNode->next = new ListNode(sum % 10);
+            pathNode = pathNode->next;
             l1 = l1->next;
         }
-        while (l2->next) {
-            tempNode->next = new ListNode(l2->val + c);
-            c = 0;
-            tempNode = tempNode->next;
+        while (l2)
+        {
+            int sum = l2->val + c;
+            c = sum / 10;
+            pathNode->next = new ListNode(sum % 10);
+            pathNode = pathNode->next;
             l2 = l2->next;
         }
-        return l3;
+        if(c)
+            pathNode->next = new ListNode(c);
+        return headNode;
     }
 };
 int main() {
-    int temp;
-    ListNode* l1 = new ListNode();
-    ListNode* rootNode1 = &(*l1);
-    ListNode* l2 = new ListNode();
-    ListNode* rootNode2 = &(*l2);
-    while (cin >> temp)
-    {
-        l1 = new ListNode(temp);
-        l1 = l1->next;
-        if (cin.get() == '\n')
-            break;
-    }
-    while (cin >> temp)
-    {
-        l2 = new ListNode(temp);
-        l2 = l2->next;
-        if (cin.get() == '\n')
-            break;
-    }
+    // ListNode* l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+    // ListNode* l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+    //ListNode* l1 = new ListNode(0);
+    //ListNode* l2 = new ListNode(0);
+    ListNode* l1 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9,
+        new ListNode(9, new ListNode(9)))))));
+    ListNode* l2 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))));
     Solution S;
-    ListNode* l3 = S.addTwoNumbers(rootNode1, rootNode2);
-    while (l3->next) {
+    ListNode* l3 = S.addTwoNumbers(l1, l2);
+    while (l3) {
         cout << l3->val << " ";
         l3 = l3->next;
     }
-    cout << endl;
     return 0;
 }
