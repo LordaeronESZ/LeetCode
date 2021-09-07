0e1d1cb71ca9ccf729d1b1f32fd06ede3819426d
超群 郑 <1667510710@qq.com>
2021-09-07 16:03:19 +0800

    2021-09-07

---
+++ 0069x的平方根/x的平方根.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 1)
+            return 1;
+        int l = 0, r = x;
+        while (l + 1 < r) {
+            int mid = (l + r) / 2;
+            if ((long)mid * mid == x)
+                return mid;
+            else if ((long)mid * mid < x)
+                l = mid;
+            else
+                r = mid;
+        }
+        return l;
+        
+    }
+};
+int main() {
+    int x = 224;
+    Solution S;
+    int res = S.mySqrt(x);
+    cout << res << endl;
+    return 0;
+}
