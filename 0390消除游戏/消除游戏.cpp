bf742003a7ab3020ee0f2fcd4003c121fb6e68f2
超群 郑 <1667510710@qq.com>
2022-01-04 19:06:17 +0800

    2022-01-04

--- 0390消除游戏/消除游戏.cpp
+++ 0390消除游戏/消除游戏.cpp
@@ -3,9 +3,26 @@ using namespace std;
 class Solution {
 public:
     int lastRemaining(int n) {
-
+        int a1 = 1;
+        int k = 0, cnt = n, step = 1;
+        while (cnt > 1) {
+            if (k % 2 == 0) { // 正向
+                a1 = a1 + step;
+            }
+            else { // 反向
+                a1 = (cnt % 2 == 0) ? a1 : a1 + step;
+            }
+            k++;
+            cnt = cnt >> 1;
+            step = step << 1;
+        }
+        return a1;
     }
 };
 int main() {
+    Solution S;
+    int n = 10;
+    auto res = S.lastRemaining(n);
+    cout << res << endl;
     return 0;
 }
